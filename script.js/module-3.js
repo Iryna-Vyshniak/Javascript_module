f52ab0bc44979daba3c4def35c155147a57550c1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-03 23:15:55 +0300

    add 17 task

--- script.js/module-3.js
+++ script.js/module-3.js
@@ -272,3 +272,34 @@ const values10 = Object.values(apartment);
 
 // 16
 // Напиши функцію countTotalSalary(salaries), яка приймає об'єкт зарплат, де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата. Функція повинна розрахувати загальну суму зарплат співробітників і повернути її. Використовуй змінну totalSalary для зберігання загальної суми зарплати.
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+const value = [];
+  for (const value of Object.values(salaries)){
+    totalSalary += value;
+  }
+  // Change code above this line
+  return totalSalary;
+}
+
+
+// 17
+// Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// const values = Object.values(colors);
+// for (const value of values) {
+//   hexColors.push(value.hex);
+//   rgbColors.push(value.rgb);
+// }
+
