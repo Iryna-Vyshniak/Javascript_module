ea6ed62ebad227442c012680ca7c289cf4de0341
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-09 10:52:25 +0300

    add script to cart

--- script.js/cart.js
+++ script.js/cart.js
@@ -10,9 +10,24 @@ const cart = {
     items: [],
     getItems() {
         return this.items;
-        console.log(getItems(product));
      },
-    addProduct(product) { },
+    addProduct(product) {
+        const { items } = this.items;
+
+        for (const item of items) {
+            console.log(item);
+            if (item.name === product.name) {
+                console.log(item);
+                console.log('this product: product.name is');
+                item.quatity += 1;
+            }
+        }
+        const newProduct = {
+            ...product,
+            quantity: 1,
+        }
+        
+     },
     removeProduct() { },
     clearCart() { },
     countTotalPrice() { },
