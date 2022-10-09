a133be7e24441f0eef80362736e732b42c8d582d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-09 12:20:05 +0300

    add 41 task script

--- script.js/module-3.js
+++ script.js/module-3.js
@@ -970,4 +970,51 @@ const atTheOldToad = {
 //   price: 700
 // }
 // Виконай рефакторинг методів об'єкта atTheOldToad таким чином, щоб вони працювали не з масивом рядків, а з масивом об'єктів.
+// this script CRAZY!!!!!
 
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+  
+//    getPotions() {
+//       const { potions } = this;
+//       return potions;
+//   },
+//     addPotion(newPotion) {
+//         const { potions } = this;
+//         for (const potion of potions) {
+            
+//             if (potion.name === newPotion.name) {
+//                 return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//             }
+//         }
+//         potions.push(newPotion);
+//     },
+
+//     removePotion(potionName) {
+//         const { potions } = this;
+        
+//       for (let i = 0; i < potions.length; i += 1){
+//           const { name } = this.potions[i];
+          
+//           if (potionName === name) {
+//               potions.splice(i, 1);
+//         }
+//       }
+//   },
+
+//     updatePotionName(oldName, newName) {
+//         const { potions } = this;
+//         for (const potion of potions) {
+//             if (potion.name === oldName) {
+//                 potion.name = newName;
+//             }
+//         }
+//   },
+
+// };
+
+// OR
