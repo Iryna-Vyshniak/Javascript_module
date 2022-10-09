76d1f2541d539a01ed07aa9b5f0315b225cc07e4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-09 12:12:29 +0300

    update cart script

--- script.js/cart.js
+++ script.js/cart.js
@@ -1,36 +1,85 @@
 const product = [
-    { name: 'apple', price: 50, },
-    { name: 'lemon', price: 60, },
-    { name: 'banana', price: 30, },
-    { name: 'mango', price: 100, },
-    { name: 'ananas', price: 150, } 
-]
+    { name: 'apple', price: 50 },
+    { name: 'lemon', price: 60 },
+    { name: 'banana', price: 30 },
+    { name: 'mango', price: 100 },
+    { name: 'ananas', price: 150 }
+];
+
+console.log(product);
 
 const cart = {
     items: [],
+    // return array items
     getItems() {
         return this.items;
-     },
+    },
+    // add products to cart
     addProduct(product) {
-        const { items } = this.items;
-
-        for (const item of items) {
-            console.log(item);
+    
+        for (const item of this.items) {
             if (item.name === product.name) {
-                console.log(item);
-                console.log('this product: product.name is');
-                item.quatity += 1;
-            }
-        }
-        const newProduct = {
-            ...product,
-            quantity: 1,
-        }
+               
+        item.quantity += 1;
+        return;
+      }
+    }
+
+    const newProduct = {
+      ...product,
+      quantity: 1,
+    };
         
-     },
-    removeProduct() { },
-    clearCart() { },
-    countTotalPrice() { },
+    this.items.push(newProduct);
+    },
+    
+    removeProduct(productName) { 
+      const { items } = this;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const {name} = this.items[i];
+
+      if (productName === name) {
+        console.log('нашли такой продукт ', productName);
+        console.log('индекс: ', i);
+
+        items.splice(i, 1);
+      }
+    }
+    },
+    clearCart() { 
+        this.items = [];
+    },
+    countTotalPrice() { 
+        const { items } = this;
+        let total = 0;
+
+        for (const {price, quantity} of items) {
+            total += price * quantity;
+        }
+        return total;
+    },
     increaseQuantity() { },
     decreaseQuantity() { },
 };
+
+console.log(cart.getItems());
+cart.addProduct({name: 'apple', price: 50 });
+cart.addProduct({name: 'lemon', price: 60 });
+cart.addProduct({name: 'banana', price: 30 });
+cart.addProduct({name: 'mango', price: 100 });
+cart.addProduct({name: 'ananas', price: 150});
+cart.addProduct({name: 'ananas', price: 150});
+cart.addProduct({name: 'ananas', price: 150});
+
+cart.removeProduct('lemon');
+console.table(cart.getItems());
+
+console.table(cart.getItems());
+
+console.log('Total: ', cart.countTotalPrice());
+
+// cart.clear();
+// console.log(cart.getItems());
+
+console.log('Total: ', cart.countTotalPrice());
