b419171b6e54b6d1c62bf23c4dd637f81bd90237
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-12 16:43:49 +0200

    add script for get discount total spent

--- app/00_practic.js
+++ app/00_practic.js
@@ -686,3 +686,32 @@ isNumberNotInRange(10, 30, 17);
 isNumberNotInRange(10, 30, 5);
 isNumberNotInRange(20, 50, 24);
 isNumberNotInRange(20, 50, 76);
+
+//get discount total spent - визначає значення знижки, залежно від загальної суми витрачених грошей  в магазині за весь час (партнерська програма). Знижка записується у змінну discount і повертається з функції як результат її роботи.
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+
+  if (totalSpent >= 50000) {
+    discount = GOLD_DISCOUNT;
+  } else if (totalSpent >= 20000 && totalSpent <= 50000) {
+    discount = SILVER_DISCOUNT;
+  } else if (totalSpent >= 5000 && totalSpent <= 20000) {
+    discount = BRONZE_DISCOUNT;
+  } else {
+    discount = BASE_DISCOUNT;
+  }
+  console.log(discount);
+  return discount;
+}
+getDiscount(137000);
+getDiscount(46900);
+getDiscount(8250);
+getDiscount(1300);
+getDiscount(5000);
+getDiscount(20000);
+getDiscount(50000);
