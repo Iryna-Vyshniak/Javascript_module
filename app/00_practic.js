cd7ba65922247c292953ef3d18e2dc19d75e86bd
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-12 21:19:06 +0200

    add script for check for spam

--- app/00_practic.js
+++ app/00_practic.js
@@ -889,3 +889,20 @@ checkForName('Egor Kolbasov', 'Egor');
 checkForName('Egor Kolbasov', 'egor');
 checkForName('Egor Kolbasov', 'egOr');
 checkForName('Vadim Nekrasov', 'Vadim');
+
+//check for spam => приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути у довільному регістрі, наприклад SPAM або sAlE.
+function checkForSpam(message) {
+  let result;
+  const normalizeMessage = message.toLowerCase();
+  result = normalizeMessage.includes('spam') || normalizeMessage.includes('sale');
+
+  console.log(result);
+  return result;
+}
+checkForSpam('Latest technology news');
+checkForSpam('JavaScript weekly newsletter');
+checkForSpam('Get best sale offers now!');
+checkForSpam('Amazing SalE, only tonight!');
+checkForSpam('Trust me, this is not a spam message');
+checkForSpam('Get rid of sPaM emails. Our book in on sale!');
+checkForSpam('[SPAM] How to earn fast money?');
