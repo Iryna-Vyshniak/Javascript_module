283cc6fbdfa40a8d9ee8bcdf758ede8948d4e5b3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-14 20:53:21 +0200

    add func to determine which phrase the girls would say at the last petal for a flower of a given number of petals

--- app/00_practic.js
+++ app/00_practic.js
@@ -1258,3 +1258,33 @@ const abbrName = name =>
 console.log(abbrName('Sam Harris'));
 console.log(abbrName('Patrick Feenan'));
 console.log(abbrName('Evan Cole'));
+
+//Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+/*
+"I love you"
+"a little"
+"a lot"
+"passionately"
+"madly"
+"not at all"
+If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+*/
+
+function howMuchILoveYou(nbPetals) {
+  let message;
+  let index;
+  let petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
+  for (let i = 0; i < nbPetals; i += 1) {
+    index = i % petals.length;
+  }
+  message = petals[index];
+  console.log(message);
+  return message;
+}
+howMuchILoveYou(3); // a lot
+howMuchILoveYou(6); // not at all
+howMuchILoveYou(7); // I love you
