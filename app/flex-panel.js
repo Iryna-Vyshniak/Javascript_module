28676094b7dbf3a367c93792d9f9ee034e844b99
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-17 18:54:43 +0200

    modified, add links, some styles, comments

--- app/flex-panel.js
+++ app/flex-panel.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-const panelList = document.querySelectorAll(".js-panel");
+const panelList = document.querySelectorAll('.js-panel');
 
 function onPanelClick(e) {
-    const activePanel = document.querySelector(".is-open");
+  const activePanel = document.querySelector('.is-open');
 
-    if (e.currentTarget === activePanel) {
-        activePanel.classList.toggle("is-open");
-    }
+  if (e.currentTarget === activePanel) {
+    activePanel.classList.toggle('is-open');
+  }
 
-    if (activePanel) {
-        activePanel.classList.remove("is-open");
-    }
+  if (activePanel) {
+    activePanel.classList.remove('is-open');
+  }
 
-    e.currentTarget.classList.add("is-open");
+  e.currentTarget.classList.add('is-open');
 }
 
-panelList.forEach((panel) => {
-    panel.addEventListener("click", onPanelClick);
+panelList.forEach(panel => {
+  panel.addEventListener('click', onPanelClick);
 });
