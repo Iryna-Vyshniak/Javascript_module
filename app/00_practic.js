b093bbb7e88adf2ce2de6cafa9a0407506f7835a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-18 18:39:22 +0200

    add next variant func with two arguments that will return an array of the first n multiples of x

--- app/00_practic.js
+++ app/00_practic.js
@@ -1864,6 +1864,19 @@ function countBy(x, n) {
 countBy(1, 10); // [1,2,3,4,5,6,7,8,9,10]
 countBy(2, 5); // [2,4,6,8,10]
 
+//next variant
+function countNumBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i += 1) {
+    z.push(i * x);
+  }
+  console.log(z);
+  return z;
+}
+
+countNumBy(1, 10); // [1,2,3,4,5,6,7,8,9,10]
+countNumBy(2, 5); // [2,4,6,8,10]
+
 // OBJECTS
 // const playlist = {
 //   name: 'My super playlist',
