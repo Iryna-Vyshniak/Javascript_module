d455629dd23253804b074a5f1b34e180a927510b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-12-27 20:30:20 +0200

    add function looks for a product object with this name (name property) in the products array and returns its price (price property)

--- app/06_object.js
+++ app/06_object.js
@@ -878,3 +878,57 @@ function countTotalSalary(salaries) {
 countTotalSalary({}); // 0
 countTotalSalary({ mango: 100, poly: 150, alfred: 80 }); // 330
 countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }); // 400
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+// 1st variant
+const valuesColors = Object.values(colors);
+for (const value of valuesColors) {
+  hexColors.push(value.hex);
+  rgbColors.push(value.rgb);
+}
+console.log('hex colors: ', hexColors);
+console.log('rgb colors: ', rgbColors);
+
+// 2nd variant
+const hexColors1 = [];
+const rgbColors1 = [];
+const keysColors = Object.keys(colors);
+
+for (const key of keysColors) {
+  hexColors1.push(colors[key].hex);
+  rgbColors1.push(colors[key].rgb);
+}
+
+console.log('hex colors: ', hexColors1);
+console.log('rgb colors: ', rgbColors1);
+
+//The function looks for a product object with this name (name property) in the products array and returns its price (price property). If no product with that name is found, the function should return null.
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price;
+    }
+  }
+  return null;
+}
+
+getProductPrice('Radar');
+console.log(getProductPrice('Radar')); //1300
+console.log(getProductPrice('Grip')); //1200
+console.log(getProductPrice('Engine')); //null
