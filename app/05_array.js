8239bb25a314fa77da8f5ff78183793846000b14
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-06 20:24:38 +0200

    codewars: add arrow function to return the additive inverse of each

--- app/05_array.js
+++ app/05_array.js
@@ -522,3 +522,11 @@ console.log(minMax([1, 2, 3, 4, 5]));
 minMax([1, 2, 3, 4, 5]);
 minMax([2334454, 5]); // [5,2334454]
 minMax([1]); // [1,1]
+
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+const invert = array => array.map(i => i * -1);
+
+console.log(invert([1, 2, 3, 4, 5])); // [-1, -2, -3, -4, -5]
+console.log(invert([1, -2, 3, -4, 5])); // [-1, 2, -3, 4, -5]
+console.log(invert([])); // []
+console.log(invert([0])); // [-0]
