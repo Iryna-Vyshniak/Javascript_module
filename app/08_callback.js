bab97e2b7a94e0fd10ac3caecbc0fdee3ff1644d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-07 14:36:19 +0200

    add callback function to filter array

--- app/08_callback.js
+++ app/08_callback.js
@@ -767,3 +767,59 @@ max(1, 2, [3, [4]]); // 4
 min(1, 2, 3, 4); // 1
 min([1, 2], 3, 4); // 1
 min(1, 2, [3, [4, 0]]); // 0
+
+// navigator
+window.navigator.geolocation.getCurrentPosition(onGetPositionsSuccess, onGetPositionsError);
+
+function onGetPositionsSuccess(position) {
+  console.log('success: ', position);
+}
+function onGetPositionsError(error) {
+  console.log('error: ', error);
+}
+
+const fruitsCart = [
+  {
+    name: 'Apple',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/265px-Red_Apple.jpg',
+    price: 35,
+  },
+  {
+    name: 'Banana',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/320px-Bananas_white_background_DS.jpg',
+    price: 12,
+  },
+  {
+    name: 'Grapes',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Table_grapes_on_white.jpg/320px-Table_grapes_on_white.jpg',
+    weight: 0.1,
+    price: 45,
+  },
+  {
+    name: 'Pineapple',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/286px-Pineapple_and_cross_section.jpg',
+    price: 200,
+  },
+];
+
+const filterItem = (array, test) => {
+  const arrFilter = [];
+
+  for (const item of array) {
+    const passed = test(item);
+    if (passed) {
+      arrFilter.push(item);
+    }
+  }
+  return arrFilter;
+};
+
+function getFruitPrice(fruit) {
+  return fruit.price >= 30;
+}
+
+console.log(filterItem(fruitsCart, getFruitPrice)); //
