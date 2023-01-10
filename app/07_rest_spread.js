668c587f0b4d877bb44d44341a68a9fbbdb7e45a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-10 23:13:04 +0200

    add object with methods

--- app/07_rest_spread.js
+++ app/07_rest_spread.js
@@ -395,3 +395,33 @@ function foo({ username } = {}) {
 
 foo(); //undefined
 foo({ username: 'Bob' });
+
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    const { potions } = this;
+    // for (let i = 0; i < potions.length; i += 1) {
+    //   if (potions[i].name === newPotion.name) {
+    //     return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    //   }
+    // }
+
+    for (const potion of potions) {
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+    return this.potions;
+  },
+};
+console.log(atTheOldToad.addPotion({ name: 'Invisibility', price: 620 })); // [{…}, {…}, {…}, {…}]
+console.log(atTheOldToad.addPotion({ name: 'Stone skin', price: 240 })); //Error! Potion Stone skin is already in your inventory!
