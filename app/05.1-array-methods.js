9a941c51505423db365286b240dd5992b76d5535
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-14 17:13:02 +0200

    add rule for  method forEach and some examples for use this method

---
+++ app/05.1-array-methods.js
@@ -0,0 +1,48 @@
+// forEach => likes loop for..for
+/*
+ * Array.prototype.forEach(callback(currentValue, index, array), thisArg)
+ * - Поелементо перебирає оригінальний масив
+ * - Нічого не повертає
+ * - Замінює класичний for, якщо не потрібно переривати цикл
+ 
+ * - element by element iterates over the original array
+ * - Returns nothing
+ * - Replaces the classic for if you don't need to break the loop
+ *
+ */
+const numbers = [5, 10, 15, 20, 25];
+
+numbers.forEach(number => {
+  console.log('number', number);
+  // number 5
+  // number 10
+  // number 15
+  // number 20
+  // number 25
+});
+
+console.log(numbers); // [5, 10, 15, 20, 25]
+
+numbers.forEach(number => {
+  console.log('number', number * 2);
+  // number 10
+  // number 20
+  // number 30
+  // number 40
+  // number 50
+});
+
+console.log(numbers); // [5, 10, 15, 20, 25]
+
+const newNumbers = numbers.forEach(number => {
+  console.log('number', number * 2);
+  // number 10
+  // number 20
+  // number 30
+  // number 40
+  // number 50
+  return number * 2; // undefined
+});
+
+console.log(numbers); // [5, 10, 15, 20, 25]
+console.log(newNumbers); // undefined
