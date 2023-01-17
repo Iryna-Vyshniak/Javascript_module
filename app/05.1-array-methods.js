1eaf28f49da54be73109be92772637554b7ebf7f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-18 01:51:44 +0200

    add some functions to use Array.prototype.filter()

--- app/05.1-array-methods.js
+++ app/05.1-array-methods.js
@@ -359,6 +359,107 @@ console.log(booksByAuthor);
 // {title: 'The Last Kingdom', author: 'Bernard Cornwell', rating: 8.38}
 // {title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67}
 
+//
+const people = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39,
+  },
+];
+const getUsersWithFriend = (users, friendName) =>
+  users.filter(user => user.friends.includes(friendName));
+getUsersWithFriend(people, 'Briana Decker');
+console.log(getUsersWithFriend(people, 'Briana Decker'));
+
+//
+const getUsersWithAge = (users, minAge, maxAge) =>
+  users.filter(({ age }) => minAge <= age && age <= maxAge);
+getUsersWithAge(people, 18, 36);
+console.log(getUsersWithAge(people, 18, 36)); //
+
+//
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+getUsersWithEyeColor(people, 'blue');
+
+//
+const getFriends = users => {
+  const allFriends = users.flatMap(user => user.friends);
+  return allFriends.filter((friend, index, array) => array.indexOf(friend) === index);
+};
+getFriends(people);
+
+//
+const getActiveUsers = users => users.filter(user => user.isActive);
+// const getActiveUsers = users => users.filter(({ isActive }) => isActive);
+
+//
+
 /*
  * Get an array of all online players
  */
