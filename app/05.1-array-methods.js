1556bb9faad1eeea7ec292bce1b42692749113e4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-18 16:53:24 +0200

    add some functions with method chaining

--- app/05.1-array-methods.js
+++ app/05.1-array-methods.js
@@ -1410,3 +1410,98 @@ students.map(student => student.courses);
 
 students.flatMap(student => student.courses);
 // ["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+
+// ===================================================================================================================
+// CHAINING
+// функція getNamesSortedByFriendCount(users) повертає масив імен користувачів, відсортований за зростанням кількості їхніх друзів (властивість friends).
+
+const getNamesSortedByFriendCount = users =>
+  [...users]
+    .sort((user1, user2) => (user1.friends.length = user2.friends.length))
+    .map(user => user.name);
+
+getNamesSortedByFriendCount(people);
+console.log(getNamesSortedByFriendCount(people));
+
+// повернути масив унікальних імен друзів (властивість friends), відсортований за алфавітом.
+const getSortedFriends = users =>
+  users
+    .flatMap(user => user.friends)
+    .filter((friend, index, array) => array.indexOf(friend) === index)
+    .sort((firstFriend, secondFriend) => firstFriend.localeCompare(secondFriend));
+
+console.log(getSortedFriends(people)); //['Aisha Tran', 'Briana Decker', 'Eddie Strong', 'Goldie Gentry', 'Jacklyn Lucas', 'Jordan Sampson', 'Linda Chapman', 'Marilyn Mcintosh', 'Padilla Garrison', 'Sharron Pace']
+
+//повернути загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
+
+const usersF = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+const getTotalBalanceByGender = (users, gender) =>
+  users.filter(user => user.gender === gender).reduce((acc, { balance }) => acc + balance, 0);
+
+console.log(getTotalBalanceByGender(usersF, 'male')); // 12053
+console.log(getTotalBalanceByGender(usersF, 'female')); // 8863
