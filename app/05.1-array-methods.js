c240277999ab83c808a015ad277a929722a35c81
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-18 02:01:05 +0200

    add some functions to use Array.prototype.find()

--- app/05.1-array-methods.js
+++ app/05.1-array-methods.js
@@ -506,6 +506,50 @@ const number = allNums.find(number => number === 10);
 console.log(allNums); //[5, 10, 15, 20, 25]
 console.log(number); // 10
 
+//
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+colorPickerOptions.find(option => option.label === 'blue'); // { label: "blue", color: "#2196F3" }
+colorPickerOptions.find(option => option.label === 'pink'); // { label: "pink", color: "#E91E63" }
+colorPickerOptions.find(option => option.label === 'white'); // undefined
+
+//
+const booksB = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+];
+const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+const AUTHOR_BOOK = 'Robert Sheckley';
+
+const bookWithTitle = booksB.find(book => book.title === BOOK_TITLE);
+const bookByAuthor = booksB.find(book => book.author === AUTHOR);
+
+//
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+console.log(getUserWithEmail(people, 'blackburndotson@furnigeer.com')); //
+// {name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com', eyeColor: 'brown', friends: Array(2), isActive: false, …}
+
+//
 const playersSocers = [
   { id: 'player-1', name: 'Roberto', timePlayed: 310, points: 54, online: false },
   { id: 'player-2', name: 'Mersi', timePlayed: 470, points: 92, online: true },
