4ac1eb3bbbb880f58b05c39c2263546fe0141d12
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-21 23:37:58 +0200

    add class Car with getter and setter

--- app/12-classes.js
+++ app/12-classes.js
@@ -20,7 +20,7 @@ class Rectangle {
 }
 
 // Expression; the class is anonymous but assigned to a variable
-const Rectangle = class {
+const RectangleA = class {
   constructor(height, width) {
     this.height = height;
     this.width = width;
@@ -28,9 +28,59 @@ const Rectangle = class {
 };
 
 // Expression; the class has its own name
-const Rectangle = class Rectangle2 {
+const RectangleB = class RectangleB2 {
   constructor(height, width) {
     this.height = height;
     this.width = width;
   }
 };
+
+// CLASS CAR
+class Car {
+  static description = `It's car's description`;
+
+  constructor({ brand, model, price } = {}) {
+    this.brand = brand;
+    this._model = model;
+    this._price = price;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+
+  get model() {
+    return this._model;
+  }
+
+  set model(newModel) {
+    this._model = newModel;
+  }
+}
+
+const carInstance = new Car({
+  brand: 'Audi',
+  model: 'Q3',
+  price: 45000,
+});
+
+console.log('🚀 ~ file: 12-classes.js:67 ~ carInstance', carInstance);
+// Car {brand: 'Audi', _model: 'Q3', price: 45000}
+// brand: "Audi"
+// price: 45000
+// _model:"Q3"
+// model: "Q3"
+
+console.log(carInstance.model); //Q3
+
+carInstance.model = 'X4';
+console.log(carInstance.model); //X4
+
+console.log(carInstance.price); //45000 getter
+
+carInstance.price = 58000; // setter
+console.log(carInstance.price); //58000 getter
