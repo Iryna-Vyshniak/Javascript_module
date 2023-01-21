4abc0f734b2bcdb4df4990201a1bd81f6938cd1e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-22 00:52:32 +0200

    add class inheritance

--- app/12-classes.js
+++ app/12-classes.js
@@ -84,3 +84,91 @@ console.log(carInstance.price); //45000 getter
 
 carInstance.price = 58000; // setter
 console.log(carInstance.price); //58000 getter
+
+// ====================================================================
+// HEROES
+
+// EXTENDS
+// SUPER ()
+
+class Hero {
+  constructor({ name = 'hero', xp = 0 } = {}) {
+    this.name = name;
+    this.xp = xp;
+  }
+
+  gainXp(amount) {
+    console.log(`${this.name} получает ${amount} опыта`);
+    this.xp += amount;
+  }
+}
+
+class Warrior extends Hero {
+  constructor({ weapon, ...restProps } = {}) {
+    super(restProps);
+
+    this.weapon = weapon;
+  }
+
+  attack() {
+    console.log(`${this.name} атакует используя ${this.weapon}`);
+  }
+}
+
+class Berserk extends Warrior {
+  constructor({ warcry, ...restProps } = {}) {
+    super(restProps);
+
+    this.warcry = warcry;
+  }
+
+  babyRage() {
+    console.log(this.warcry);
+  }
+}
+
+const ajax = new Berserk({
+  name: 'ajax',
+  xp: 500,
+  weapon: 'axe',
+  warcry: 'waaaaaaaah',
+});
+
+console.log(ajax);
+
+// ajax.babyRage();
+// ajax.attack();
+// ajax.gainXp();
+
+class Mage extends Hero {
+  constructor({ spells, ...restProps } = {}) {
+    super(restProps);
+
+    this.spells = spells;
+  }
+
+  cast() {
+    console.log(`${this.name} что-то там кастует 🧙‍♂️`);
+  }
+}
+
+const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'алебарда' });
+console.log(mango);
+mango.attack();
+mango.gainXp(1000);
+
+// console.log(
+//     'mango.__proto__ === Warrior.prototype ',
+//     mango.__proto__ === Warrior.prototype,
+// );
+//  console.log(Object.getPrototypeOf(mango) === Warrior.prototype);
+
+// console.log(
+//     'Warrior.prototype.__proto__ === Hero.prototype ',
+//     Warrior.prototype.__proto__ === Hero.prototype,
+// );
+
+// console.log(
+//     'Hero.prototype.__proto__ === Object.prototype ',
+//     Hero.prototype.__proto__ === Object.prototype,
+// );
