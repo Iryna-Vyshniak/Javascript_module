0e46355b2c6edf1cfac2d995ff1a37e1086f555c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-25 02:01:11 +0200

    add class student

--- app/12-classes.js
+++ app/12-classes.js
@@ -662,3 +662,62 @@ console.log(film1);
 // new Film(3, "Last Seen Alive");
 film1.isWatched = true;
 console.log(film1.isWatched); // Film {title: 'Interceptor', #isWatched: false, #id: 1}
+
+//-------------------------------------------------------------------------
+
+// Реалізуйте клас Student, який успадковуватиметься від класу User. Цей клас повинен мати такі властивості:
+// name приватна властивість (ім'я, успадковується від User),
+// surname приватна властивість (прізвище, успадковується від User),
+// year (рік вступу до вузу).
+// Клас повинен мати метод getFullName() (успадковується від User), за допомогою якого можна вивести одночасно ім'я та прізвище студента.
+// Також клас повинен мати метод getCourse(), який виводитиме поточний курс студента (від 1 до 5, якщо значення перевищує 5  курс виводити що студент являєтсья випускником).
+// Курс обчислюється так: потрібно від поточного року відняти рік вступу до вузу. Поточний рік отримаєте самостійно (читати документацію!!!).
+// Приклад ініціалізації екземпляру класа:
+// const student = new Student('Петрик', 'Пяточкин', 2019);
+
+class UserStudent {
+  static counter = 0;
+  static addUser() {
+    this.counter += 1;
+    console.log(this.counter);
+  }
+  #name;
+  #surname;
+
+  constructor(name, surname) {
+    this.#name = name;
+    this.#surname = surname;
+    UserStudent.addUser();
+  }
+
+  getFullName() {
+    return `${this.#name} ${this.#surname}`;
+  }
+}
+
+class StudentCollege extends UserStudent {
+  constructor(name, surname, year) {
+    super(name, surname);
+
+    this.year = year;
+  }
+
+  getCourse() {
+    const currentTime = new Date().getFullYear();
+    const result = currentTime - this.year;
+    if (result <= 5) {
+      return result;
+    }
+    return `Вже випускник!`;
+  }
+}
+const student1 = new StudentCollege('Vita', '1232');
+const student2 = new StudentCollege('ghjhg', '1ghgjh');
+const user1 = new UserStudent('Vita111', 'qwe');
+const user2 = new UserStudent('Vita222', 'qtyu');
+const student = new StudentCollege('Yuliya', 'Nikiforova', 2020);
+
+console.log(student.getCourse());
+console.log(student.getFullName()); //поверне 'Петрик Пяточкин'
+console.log(student.getCourse()); //поверне 3 (третій курс)
+console.log(student); //StudentCollege {year: 2020, #name: 'Yuliya', #surname: 'Nikiforova'}
