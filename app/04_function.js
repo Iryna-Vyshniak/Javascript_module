1261d668a29c527723e5d4474e09b61a2f7ca951
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-01 15:51:22 +0200

    add closure function that make size

--- app/04_function.js
+++ app/04_function.js
@@ -637,3 +637,18 @@ function fooA() {
 
 console.log(fooA()); // Don't have arguments
 console.log(fooA(1, 2)); // 3
+
+//--------------------------------------------------------------------------------------------------------------
+function makeSizer(size) {
+  return function () {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+const size12 = makeSizer(12);
+const size14 = makeSizer(14);
+const size16 = makeSizer(16);
+
+// document.getElementById('size-12').onclick = size12;
+// document.getElementById('size-14').onclick = size14;
+// document.getElementById('size-16').onclick = size16;
