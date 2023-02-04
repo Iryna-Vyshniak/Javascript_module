68015ef5ce9340763041ea2d96ae02e083dcd5f9
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-04 17:13:06 +0200

    Create productsArr.js

---
+++ app/productsArr.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    name: 'hero Product',
+    detail: 'Lorem ipsum dolor sit amet',
+    price: '990',
+    hero: 'OMG This just came out today!',
+    image: 'https://media.very.co.uk/i/very/V8YHR_SQ1_0000016239_STARLIGHT_SLf?h=2199&w=1650',
+  },
+  {
+    name: 'Product 1',
+    detail: 'Lorem ipsum dolor sit amet',
+    price: '324',
+    info: 'This is the latest and greatest product from Derp corp.',
+    image:
+      'https://media.very.co.uk/i/very/VFM2D_SQ1_0000000004_BLACK_SLf/samsung-galaxy-s23-ultra-smartphone-512gb.jpg?$234x312_retinax2$&$roundel_very$&p1_img=memory_upgrade_512&p3_img=video_roundel&fmt=webp',
+  },
+  {
+    name: 'Product 2',
+    detail: 'Lorem ipsum dolor sit amet',
+    price: '889',
+    offer: 'BOGOF',
+    image:
+      'https://media.very.co.uk/i/very/VFM2A_SQ1_0000000162_LAVENDER_SLf/samsung-galaxy-s23-smartphone-512gb.jpg?$234x312_retinax2$&$roundel_very$&p1_img=memory_upgrade_512&p3_img=video_roundel&fmt=webp',
+  },
+  {
+    name: 'Product 3',
+    detail: 'Lorem ipsum dolor sit amet',
+    price: '567',
+    image:
+      'https://media.very.co.uk/i/very/VFM2E_SQ1_0000000004_BLACK_SLf/samsung-galaxy-s23-ultra-smartphone-256gb.jpg?$234x312_retinax2$&$roundel_very$&p1_img=trade_in_claim&p3_img=video_roundel&fmt=webp',
+  },
+  {
+    name: 'Product 4',
+    detail: 'Lorem ipsum dolor sit amet',
+    price: '2990',
+    offer: 'No srsly GTFO',
+    image:
+      'https://media.very.co.uk/i/very/V8YIZ_SQ1_0000000039_PURPLE_SLf/apple-iphone-14-pro-max-128gb--nbspdeep-purple.jpg?$234x312_retinax2$&$roundel_very$&p1_img=blank_apple&fmt=webp',
+  },
+];
