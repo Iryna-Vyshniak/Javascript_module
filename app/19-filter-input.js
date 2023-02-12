867be9af192e392850b48c973bc60d75839bf575
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-12 17:36:47 +0200

    Rendered the markup of the list items, listened to the filter change, filtered data and rendered the new items  main

--- app/19-filter-input.js
+++ app/19-filter-input.js
@@ -105,3 +105,75 @@ function renderListMarkup(items) {
 function markupList(markup) {
   refs2.list.innerHTML = markup;
 }
+
+//----------------------------------------------------------------
+const allStudents = [
+  'Jayne Kuyper',
+  'Tiesha Laine',
+  'Lupe Olague',
+  'Clarita Bellman',
+  'Laura Munden',
+  'Bertram Calvi',
+  'Georgianna Sparr',
+  'Carolann Seller',
+  'Felicia Midgette',
+  'Kasandra Bezio',
+  'Elias Vanwyk',
+  'Yu Mccool',
+  'Kati Golub',
+  'Pat Dalley',
+  'Loreta Francis',
+  'Beatris Larusso',
+  'Corrin Vancleve',
+  'Sofia Ressler',
+  'Erlene Burke',
+  'Breann Sande',
+  'Samatha Sao',
+  'Henry Say',
+  'Monnie Bernhard',
+  'Dia Culbert',
+  'Sunny Disandro',
+  'Mack Sen',
+  'Jeremiah Astle',
+  'Misha Ono',
+  'Silas Aubry',
+  'Kenya Longmore',
+  'Mirta Brandenberger',
+  'Eneida Overholt',
+  'Marcelo Popejoy',
+  'Serafina Irvin',
+  'Leroy Tacey',
+  'Brice Hedrick',
+  'Elana Hoops',
+  'Keena London',
+  'Enriqueta Hein',
+  'Valeria Turnbull',
+  'Earlean Canchola',
+  'Corinna Tousignant',
+  'Sherry Rivera',
+  'Chasity Janda',
+  'Tamela Barlow',
+  'Leandro Lonergan',
+  'Karlene Breunig',
+  'Winter Endicott',
+  'Salena Nail',
+  'Tanner Clapper',
+];
+
+const searchBox = document.querySelector('#search-box');
+const studentList = document.querySelector('.js-list-students');
+
+searchBox.addEventListener('input', _.debounce(onSearch, 300));
+
+populateStudentList(allStudents);
+
+function onSearch(e) {
+  const filter = e.target.value.toLowerCase();
+  const visibleStudents = allStudents.filter(student => student.toLowerCase().includes(filter));
+  populateStudentList(visibleStudents);
+}
+
+function populateStudentList(students) {
+  const markup = students.map(student => `<li>${student}</li>`).join('');
+  studentList.innerHTML = markup;
+}
