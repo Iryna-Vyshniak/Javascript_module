ec72385a535ec255ef80a8a7888d3a95287f92b2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-18 17:55:35 +0200

    add function to get all data and add to local storage

--- app/42-filters-LS.js
+++ app/42-filters-LS.js
@@ -1,7 +1,7 @@
 const filterForm = document.querySelector('.filter-form');
 // console.log(filterForm);
 
-filterForm.addEventListener('change', () => {});
+filterForm.addEventListener('change', onChange);
 filterForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
@@ -20,8 +20,30 @@ function onFormSubmit(e) {
   //formData.forEach(data => console.log(data)); // red, sm, steel
 
   // (method) FormData.forEach(callbackfn: (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg?: any): void
+  // key === name from <select name="color">
   formData.forEach((value, key) => console.log(`${value}: ${key}`));
   //red: color
   // sm: size
   // steel: material
+}
+
+// створюємо обєкт, де будуть збережені всі дані з нашої групи даних
+// {
+//     material: 'plastic',
+//     size: 'xl',
+//     color: 'blue',
+// }
+
+const selectedFilters = {};
+
+// отримуємо значення з кожного select при зміні вибору даних
+function onChange(e) {
+  //   console.log(e.target); // <select name="color"></select> <select name="size"></select> <select name="material"></select>
+  //   console.log(e.target.name); // color
+  //   console.log(e.target.value); // green
+  selectedFilters[e.target.name] = e.target.value;
+  // console.log(selectedFilters); // {color: 'green', size: 'xl', material: 'wood'} ->якщо ключа немає - він створиться, якщо є - зміниться значення
+  // add to local storage
+  // "[object Object]" отримуємо, коли хочемо об'єкт перетворити на рядок, тому JSON.stringify
+  localStorage.setItem('selectedFilters', JSON.stringify(selectedFilters));
 }
