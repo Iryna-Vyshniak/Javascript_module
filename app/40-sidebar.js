b26a1a348d1d866175e84289635cf0e6e0a58cfb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-18 15:01:58 +0200

    save sidebar state to localStorage

--- app/40-sidebar.js
+++ app/40-sidebar.js
@@ -1,17 +1,35 @@
+const LOCALSTORAGE_KEY = 'is-sidebar-open';
+
 const sidebar = document.querySelector('.sidebar');
-console.log(sidebar);
+
+// збережений стан бічної панелі
+const persistedSidebarState = localStorage.getItem(LOCALSTORAGE_KEY);
+if (persistedSidebarState) {
+  // JSON.parse(persistedSidebarState);
+  const shouldOpenSidebar = JSON.parse(persistedSidebarState);
+  if (shouldOpenSidebar) {
+    openSidebar();
+  }
+}
 
 document.querySelector('button[data-open-sidebar]').addEventListener('click', e => {
   openSidebar();
+  saveSidebarStateToLS(true);
 });
 
 document.querySelector('button[data-close-sidebar]').addEventListener('click', e => {
   closeSidebar();
+  saveSidebarStateToLS(false);
 });
 
 function openSidebar() {
   sidebar.classList.add('is-open');
 }
+
 function closeSidebar() {
   sidebar.classList.remove('is-open');
+}
+
+function saveSidebarStateToLS(state) {
+  localStorage.setItem(LOCALSTORAGE_KEY, state);
 }
