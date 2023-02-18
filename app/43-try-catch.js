9972e2b70eb8c00fbbd891b431e44095f96ece44
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-18 23:52:57 +0200

    add some rules for use localeStorage, JSON.stringify/parse, try...catch

---
+++ app/43-try-catch.js
@@ -0,0 +1,67 @@
+//If invalid JSON is passed to the methods of the JSON class, they will "throw" an error and the entire script will crash. To avoid this, the try...catch construct is used, which allows you to "catch" and handle script execution errors.
+/* try {
+  // Code that may throw a runtime error
+} catch (error) {
+  // Error handling
+} */
+
+//By using the try...catch construct, we can handle this exception in such a way that the script continues to run even if an error occurs.
+
+/* try {
+  const data = JSON.parse('Well, this is awkward');
+} catch (error) {
+  console.log(error.name); // "SyntaxError"
+  console.log(error.message); // Unexpected token W in JSON at position 0
+}
+
+console.log('✅ This is fine, we handled parse error in try...catch'); */
+
+//Для того, щоб скоротити кількість повторюваного коду при роботі з веб-сховищем, можна написати сервіс зі стандартними методами, наприклад, save і load. Вони будуть абстрагувати повторюваний код перевірки помилок парса і подібну рутину.
+
+/* const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+}; */
+
+const addLocal = document.querySelector('.js-addLocal');
+const getLocal = document.querySelector('.js-getLocal');
+const removeLocal = document.querySelector('.js-removeLocal');
+const addSession = document.querySelector('.js-addSession');
+const removeSession = document.querySelector('.js-removeSession');
+
+const KEY_LOCAL = 'localStorage';
+addLocal.addEventListener('click', onAddLocal);
+getLocal.addEventListener('click', onGetLocal);
+removeLocal.addEventListener('click', onRemoveLocal);
+
+function onAddLocal() {
+  const arr = [1, 2, 3, 5];
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(arr));
+}
+
+function onGetLocal() {
+  // в змінну result зберегли результат виконання парсу, який отримали з localStorage по ключу KEY_LOCAL
+  const result = JSON.parse(localStorage.getItem(KEY_LOCAL));
+  console.log(result);
+}
+function onRemoveLocal() {
+  localStorage.removeItem(KEY_LOCAL);
+}
