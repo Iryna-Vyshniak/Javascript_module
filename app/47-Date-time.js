2254b384bec88a981127f96d040f4eaec7661d39
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-25 17:31:19 +0200

    add script for simple stopwatch

--- app/47-Date-time.js
+++ app/47-Date-time.js
@@ -45,3 +45,54 @@ const timer = targetDate => {
 
 const renderTimer = str => (document.querySelector('.simple-timer').innerText = str);
 timer(new Date('2023/02/26')); // mseconds from 01/01/1970 to 2023/02/25 => for ex: 23345678
+
+// --- STOPWATCH --------------------------------
+const stopBtn = document.querySelector('.js-stop');
+const startBtn = document.querySelector('.js-start');
+
+let intervalId;
+
+const pause = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+};
+
+const start = () => {
+  const initialDate = new Date();
+  intervalId = setInterval(() => {
+    const delta = new Date() - initialDate;
+
+    const milliseconds = Math.floor(delta % millisecondsInSec);
+    const seconds = Math.floor((delta / millisecondsInSec) % secondsInMin);
+    const minutes = Math.floor((delta / (millisecondsInSec * secondsInMin)) % minutesInHour);
+    const hours = Math.floor(
+      (delta / (millisecondsInSec * secondsInMin * minutesInHour)) % hoursInDay
+    );
+    const days = Math.floor(
+      delta / (millisecondsInSec * secondsInMin * minutesInHour * hoursInDay)
+    );
+
+    // const UISeconds = String(seconds).length === 2 ? seconds : `0${seconds}`;
+    // const UIMinutes = String(minutes).length === 2 ? minutes : `0${minutes}`;
+    // const UIHours = String(hours).length === 2 ? hours : `0${hours}`;
+
+    const UISeconds = String(seconds).padStart(2, '0');
+    const UIMinutes = String(minutes).padStart(2, '0');
+    const UIHours = String(hours).padStart(2, '0');
+
+    const stopwatcherFormat = `${days} days ${UIHours}:${UIMinutes}:${UISeconds}:${milliseconds}`;
+
+    renderStopWatch(stopwatcherFormat);
+  }, 0);
+};
+
+const renderStopWatch = str => (document.querySelector('.stopwatch').innerText = str);
+
+stopBtn.addEventListener('click', () => {
+  pause();
+});
+
+startBtn.addEventListener('click', () => {
+  start();
+});
