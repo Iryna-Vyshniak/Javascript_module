2bc23266552c04b760849377e762d7ca583ca482
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-27 12:38:04 +0200

    add functions to get last day of before months and current months

--- app/47-Date-time.js
+++ app/47-Date-time.js
@@ -221,3 +221,27 @@ refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 function updateClockface({ hours, mins, secs }) {
   refs.clockface.textContent = `${hours}:${mins}:${secs}`;
 }
+//TODO ----------------------------------------------------------------
+// get last day of before months
+
+const getLastDayOfBeforeMonths = (year, months) => {
+  // const date = new Date(year, months, 0); // Tue Jan 31 2023 00:00:00 GMT+0200 (Eastern European Standard Time)
+  // const date = new Date(year, months, -1); //Mon Jan 30 2023 00:00:00 GMT+0200 (Eastern European Standard Time)
+  const date = new Date(year, months, 0);
+  console.log(date);
+};
+
+getLastDayOfBeforeMonths(2023, 2); // Tue Feb 28 2023 00:00:00 GMT+0200
+getLastDayOfBeforeMonths(2022, 2); //Mon Feb 28 2022 00:00:00 GMT+0200
+getLastDayOfBeforeMonths(2020, 2); // Sat Feb 29 2020 00:00:00 GMT+0200
+getLastDayOfBeforeMonths(2012, 1); // Tue Jan 31 2012 00:00:00 GMT+0200
+
+const getLastDayOfMonths = (year, months) => {
+  const date = new Date(year, months + 1, 0);
+  console.log(date);
+};
+
+getLastDayOfMonths(2023, 2); // Fri Mar 31 2023 00:00:00 GMT+0300
+getLastDayOfMonths(2022, 2); //Thu Mar 31 2022 00:00:00 GMT+0300
+getLastDayOfMonths(2020, 2); // Tue Mar 31 2020 00:00:00 GMT+0300
+getLastDayOfMonths(2012, 1); // Wed Feb 29 2012 00:00:00 GMT+0200
