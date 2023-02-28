750422d442273afbef3032c4daac17db04aa5276
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-03-01 01:52:42 +0200

    add simple markup for horse racetrack

---
+++ app/53-racetrack.js
@@ -0,0 +1,4 @@
+const startBtn = document.querySelector('.js-start-race');
+const winnerField = document.querySelector('.js-winner');
+const progressField = document.querySelector('.js-progress');
+const tableBody = document.querySelector('.js-results-table > tbody');
